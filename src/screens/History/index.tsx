3d50fd62196fe236b904b1abec82999837437395
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-19 12:42:18 -0500

    feat: add RN gesture handler. Used Swipeable comp

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
-import { HouseLine } from 'phosphor-react-native';
+import { View, ScrollView, Alert} from 'react-native';
+import { HouseLine , Trash} from 'phosphor-react-native';
+import { THEME } from '../../styles/theme';
 
 import { Header } from '../../components/Header';
 import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
@@ -10,12 +11,17 @@ import { styles } from './styles';
 import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
 import { Loading } from '../../components/Loading';
 
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
 import Animated , { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated';
-const TouchableOpacityAnimated = Animated.createAnimatedComponent(TouchableOpacity)
+
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
   const [history, setHistory] = useState<HistoryProps[]>([]);
+  
+  const swipeableRefs= useRef<Swipeable[]>([]);
+
 
   const { goBack } = useNavigation();
 
@@ -31,7 +37,11 @@ export function History() {
     fetchHistory();
   }
 
-  function handleRemove(id: string) {
+  function handleRemove(id: string, index: number) {
+   // accessing a History item directly and setting it to close whenever the trash icon(Swipeable) is clicked
+   // this way it closes and the Alert is displayed
+  swipeableRefs.current?.[index].close();
+
     Alert.alert(
       'Remover',
       'Deseja remover esse registro?',
@@ -67,23 +77,39 @@ export function History() {
         showsVerticalScrollIndicator={false}
       >
         {
-          history.map((item) => (
-            // <Animated.View
-            //   key={item.id}
-            //   layout={Layout.springify()}
-            //   entering={SlideInRight}
-            //   exiting={SlideOutRight}
-            // >
-              <TouchableOpacityAnimated
-               layout={Layout.springify()}
-               entering={SlideInRight}
-               exiting={SlideOutRight}
-                key={item.id}
-                onPress={() => handleRemove(item.id)}
-              >
-                <HistoryCard data={item} />
-              </TouchableOpacityAnimated>
-            // </Animated.View>
+          history.map((item, index) => (
+            <Animated.View
+              key={item.id}
+              layout={Layout.springify()}
+              entering={SlideInRight}
+              exiting={SlideOutRight}
+            >
+                <Swipeable 
+                  ref={(ref)=>{
+                    if(ref){
+                      swipeableRefs.current.push(ref)
+                 
+                    }
+                  }}
+                    containerStyle={styles.swipableContainer}
+                    overshootLeft={false}
+                    // renderRightActions={()=>null}
+                    onSwipeableOpen={()=>handleRemove(item.id, index)}
+                    leftThreshold={10}
+                    renderLeftActions={()=>
+                    <View 
+                      style={styles.swipableRemove}
+                      // onPress={()=>handleRemove(item.id, index)}
+                      >
+                      <Trash color={THEME.COLORS.GREY_100} size={32}/>
+                  </View>
+                }
+             
+                >
+                  <HistoryCard data={item} />
+                </Swipeable>
+         
+           </Animated.View>
           ))
         }
       </ScrollView>
--- src/screens/History/styles.ts
+++ src/screens/History/styles.ts
@@ -4,10 +4,27 @@ import { THEME } from '../../styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: THEME.COLORS.GREY_800,
   },
   history: {
+    flexGrow:1,// helps space at the bottom of the list to grow when item is deleted
     padding: 32,
+  },
+  swipableRemove:{
+    width: 90, 
+    height:90, 
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+  },
+  swipableContainer:{
+    width: '100%',
+    height:90, 
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    marginBottom: 12
+
   }
 });
