59aac4aa73628c7702713300bcec438b69ef87cd
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-21 08:47:51 -0500

    feat: draw a circle with path.addCircle inside the Options component and animated it.

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,13 +1,43 @@
+import { useEffect } from 'react';
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
-
 import { styles } from './styles';
+import { THEME } from '../../styles/theme';
+import { 
+    Canvas, 
+    Skia, 
+    Path, 
+    useValue, 
+    runTiming, 
+    BlurMask 
+  } from '@shopify/react-native-skia';
 
 type Props = TouchableOpacityProps & {
   checked: boolean;
   title: string;
 }
 
+const CHECK_SIZE = 28;
+const CHECK_STROKE = 2;
+
+
 export function Option({ checked, title, ...rest }: Props) {
+
+  const RADIUS = (CHECK_SIZE - CHECK_STROKE)/2;
+  const percentage = useValue(0);
+
+  const path = Skia.Path.Make();
+  path.addCircle( CHECK_SIZE, CHECK_SIZE, RADIUS );
+
+
+  useEffect(()=>{
+    if(checked){
+      runTiming(percentage, 1, { duration: 700 });
+    }else{
+      runTiming(percentage, 0 , { duration: 700});
+    }
+  },[checked]);
+
+
   return (
     <TouchableOpacity
       style={
@@ -21,6 +51,26 @@ export function Option({ checked, title, ...rest }: Props) {
       <Text style={styles.title}>
         {title}
       </Text>
+      <Canvas style={{height: CHECK_SIZE * 2, width: CHECK_SIZE * 2}}>
+        <Path 
+          path={path}
+          color={THEME.COLORS.GREY_500}
+          style='stroke'
+          strokeWidth={CHECK_STROKE}
+          />
+
+          <Path 
+            path={path}
+            color={THEME.COLORS.BRAND_LIGHT}
+            style='stroke'
+            strokeWidth={CHECK_STROKE}
+            start={0}
+            end={percentage}
+            >
+            <BlurMask style='solid' blur={1}/>
+
+            </Path>
+      </Canvas>
     </TouchableOpacity>
   );
 }
