03ebccc55e62be9b2eaa18aa130433d7db6e3fd4
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-21 13:24:39 -0500

    feat: add Stars component

--- src/screens/Finish/index.tsx
+++ src/screens/Finish/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { Button } from '../../components/Button';
+import { Stars } from '../../components/Stars';
 
 import { styles } from './styles';
 
@@ -9,6 +10,7 @@ interface Params {
   total: string;
   points: string;
 }
+
 
 export function Finish() {
   const route = useRoute();
@@ -19,9 +21,13 @@ export function Finish() {
   return (
     <View style={styles.container}>
       <View style={styles.message}>
+
+        <Stars/>
+
         <Text style={styles.title}>
           Parabéns!
         </Text>
+
 
         <Text style={styles.subtitle}>
           Você acertou {points} de {total} questões
